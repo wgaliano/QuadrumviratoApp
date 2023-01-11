a417a862bad3e16da3d9dbbf987e472479cd04bc
wgaliano <wgaliano22@fed.idserve.net>
2023-01-11 16:32:47 +0100

    Initial Commit

---
+++ QuadrumviratoApp/QuadrumviratoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadrumviratoAppApp.swift
+//  QuadrumviratoApp
+//
+//  Created by Walter Galiano on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct QuadrumviratoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
