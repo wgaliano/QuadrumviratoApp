609f3e7360e041ddaf1688dbeeb77e0a356845cd
wgaliano <wgaliano22@fed.idserve.net>
2023-01-23 17:10:49 +0100

    comments deleted in the FeaturedCardView

--- QuadrumviratoApp/Views/Card/FeaturedCardView.swift
+++ QuadrumviratoApp/Views/Card/FeaturedCardView.swift
@@ -34,48 +34,8 @@ struct FeaturedCardView: View {
                 
                 
             }
-            .offset(x: offset.width * 1, y: offset.height * 0.4)
-//        .rotationEffect(.degrees(Double(offset.width / 40)))
-//        .gesture(
-//            DragGesture()
-//                .onChanged { gesture in
-//                    offset = gesture.translation
-//                    withAnimation {
-//                        changeColor(width: offset.width)
-//                    }
-//                }
-//                .onEnded { _ in
-//                    withAnimation {
-//                        swipeCard(width: offset.width)
-//                        changeColor(width: offset.width)
-//                    }
-//                }
-//        )
+            //.offset(x: offset.width * 1, y: offset.height * 0.4)
     }
-    
-//    func swipeCard(width: CGFloat) {
-//        switch width {
-//        case -500...(-150):
-//            print("\(String(describing: hit?.recipe.label)) removed")
-//            offset = CGSize(width: -500, height: 0)
-//        case 150...500:
-//            print("\(String(describing: hit?.recipe.label)) added")
-//            offset = CGSize(width: 500, height: 0)
-//        default:
-//            offset = .zero
-//        }
-//    }
-//
-//    func changeColor(width: CGFloat) {
-//        switch width {
-//        case -500...(-130):
-//            color = .red
-//        case 130...500:
-//            color = .green
-//        default:
-//            color = .black
-//        }
-//    }
 }
 
 //struct FeaturedCardView_Previews: PreviewProvider {
